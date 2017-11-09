f0f9e8594ce8588801b02c4a02c2f99445a661c8
Sharon Njihia <sharonnjihia@Sharons-MacBook-Pro.local>
2017-11-09 15:23:41 +0300

    (Users): add functionality for getting one user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,31 @@
 let mongoose = require('mongoose');
 let Users = mongoose.model('Users');
 
-exports.create_new_user = function(request, response) {
+
+exports.create_new_user = (request, response) => {
     let newUser = new Users(request.body);
     newUser.save((error,user)=> {
         if(error){
             response.send(error);
         }
         response.json(user);
-    })
+    });
+};
 
+exports.get_all_users = (request, response) => {
+    Users.find({}, (error, users)=> {
+        if(error) {
+            response.send(error);
+        }
+        response.json(users)
+    });
+};
+
+exports.get_one_user = (request, response) => {
+    Users.find({}, { name: request.params.username}, (error, user)=>{
+        if(error) {
+            response.send(error)
+        }
+        response.json(user)
+    });
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,8 @@ const routes = require('express').Router();
 
 //user routes
         routes
-            .post('/register', users.create_new_user);
+            .post('/register', users.create_new_user)
+            .get('/get-users', users.get_all_users)
+            .get('/get-oneuser/:username', users.get_one_user);
 
 module.exports = routes;
